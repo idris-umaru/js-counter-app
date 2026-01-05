dbe9b8c1b717c48906ba73e7529e94b8e4452641
umaruidris <idrisumaru123@gamil.com>
2026-01-05 12:59:35 +0100

    implement calculator logic with reusable input function

--- calculator app/app.js
+++ calculator app/app.js
@@ -5,28 +5,39 @@ const substract = document.getElementById("subtract");
 const multiply = document.getElementById("multiply");
 const divide = document.getElementById("divide");
 let result;
-//Add event listerner to buttons 
-add.addEventListener("click", ()=> {
+//function to call the num1 insated of calling it mutiple times
+ function getNumbers(){
     const number1 = Number(document.getElementById('num1').value);
     const number2 = Number(document.getElementById('num2').value);
+    
+    if (isNaN(number1) || isNaN(number2)){
+        alert("Please enter valid numbers");
+        return null;
+    }
+    
+    return {
+        number1: number1,
+        number2: number2
+    }
+}
+//Add event listerner to buttons 
+add.addEventListener("click", ()=> {
+    const {number1, number2} = getNumbers();
     result = number1 + number2;
     displayResult();    
 })
 substract.addEventListener("click", () => {
-    const number1 = Number(document.getElementById('num1').value);
-    const number2 = Number(document.getElementById('num2').value);
+    const {number1, number2} = getNumbers();
     result = number1 - number2;
     displayResult();
 });
 multiply.addEventListener("click", () => {
-    const number1 = Number(document.getElementById('num1').value);
-    const number2 = Number(document.getElementById('num2').value);
-    result = number1 * number2;
-    displayResult();
+   const {number1, number2} = getNumbers();
+   result = number1 * number2;
+   displayResult();
 });
 divide.addEventListener("click", () => {
-    const number1 = Number(document.getElementById('num1').value);
-    const number2 = Number(document.getElementById('num2').value);
+    const {number1, number2} = getNumbers();
     if (number2 !== 0) {
         result = number1 / number2;
         displayResult();
